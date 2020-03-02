cbceff36d0f7649830d425422461574571cd4f78
Tsubaki-SK <61465092+Tsubaki-SK@users.noreply.github.com>
2020-03-02 22:40:57 +0900

    Add files via upload

--- breakout.py
+++ breakout.py
@@ -8,9 +8,7 @@ import math
 
 pygame.init()
 pygame.mixer.quit()
-#pygame.mixer.pre_init(buffersize = 2048)
 pygame.mixer.pre_init(22050, 16, 2, 1024)
-#pygame.mixer.init(frequency = 44100)
 pygame.mixer.init()
 
 
@@ -40,8 +38,8 @@ class Ball:
         self.vector_x = int(self.vector_length * math.sin(self.init_angle * math.pi / 180))
         self.vector_y = int(self.vector_length * math.cos(self.init_angle * math.pi / 180))
 
-        self.vector_x_log = self.vector_x
-        self.vector_y_log = self.vector_y
+        self.vector_x_log= [self.vector_x for i in range(3)]
+        self.vector_y_log= [self.vector_y for i in range(3)]
 
         self.line_length = 30
         self.add_x = int(self.line_length * math.sin(self.init_angle * math.pi / 180))
@@ -60,15 +58,21 @@ class Ball:
 
 
     def VectorLog(self):
-        self.vector_x_log = self.vector_x
-        self.vector_y_log = self.vector_y
+
+        for i in range(2,-1,-1):
+            if i == 0:
+                self.vector_x_log[i] = self.vector_x
+                self.vector_y_log[i] = self.vector_y
+            else:
+                self.vector_x_log[i] = self.vector_x_log[i-1]
+                self.vector_y_log[i] = self.vector_y_log[i-1]
 
 
     def HitException(self):
 
         if self.hit_wall == True and self.hit_shield == True:
-            self.vector_x = -self.vector_x_log
-            self.vector_y = -self.vector_y_log
+            self.vector_x = -self.vector_x_log[2]
+            self.vector_y = -self.vector_y_log[2]
 
 
     def MoveBall(self):
@@ -170,11 +174,15 @@ class Shield:
         block_object.breaked_block.clear()
 
 
+    def ComboReset(self,ball_object):
+        return ball_object.ball.colliderect(self.shield)
+
 
 class Block:
     def __init__(self):
         self.normal_block = []
         self.breaked_block = []
+        self.combo_block = []
         self.block_num = list(range(66))
 
         self.longer_id = [20, 49]
@@ -231,50 +239,56 @@ class Block:
     def HitCorner(self, ball_object, corner_x, corner_y, hit_part):
 
         if hit_part == 'upper_left':
-            if ball_object.vector_x_log > 0 and ball_object.vector_y_log > 0: #ok
+            if ball_object.vector_x_log[0] > 0 and ball_object.vector_y_log[0] > 0: #ok
                 ball_object.vector_x *= -1
                 ball_object.vector_y *= -1
-            elif ball_object.vector_x_log <= 0 and ball_object.vector_y_log > 0: #ok
+            elif ball_object.vector_x_log[0] <= 0 and ball_object.vector_y_log[0] > 0: #ok
                 ball_object.vector_y *= -1
-            elif ball_object.vector_x_log > 0 and ball_object.vector_y_log < 0:
+            elif ball_object.vector_x_log[0] > 0 and ball_object.vector_y_log[0] < 0:
                 ball_object.vector_x *= -1
 
         if hit_part == 'upper_right':
-            if ball_object.vector_x_log < 0 and ball_object.vector_y_log > 0: #ok
+            if ball_object.vector_x_log[0] < 0 and ball_object.vector_y_log[0] > 0: #ok
                 ball_object.vector_x *= -1
                 ball_object.vector_y *= -1
-            elif ball_object.vector_x_log >= 0 and ball_object.vector_y_log > 0: #ok
+            elif ball_object.vector_x_log[0] >= 0 and ball_object.vector_y_log[0] > 0: #ok
                 ball_object.vector_y *= -1
-            elif ball_object.vector_x_log < 0 and ball_object.vector_y_log < 0: #ok
+            elif ball_object.vector_x_log[0] < 0 and ball_object.vector_y_log[0] < 0: #ok
                 ball_object.vector_x *= -1
 
         if hit_part == 'lower_right':
-            if ball_object.vector_x_log < 0 and ball_object.vector_y_log < 0: #ok
+            if ball_object.vector_x_log[0] < 0 and ball_object.vector_y_log[0] < 0: #ok
                 ball_object.vector_x *= -1
                 ball_object.vector_y *= -1
-            elif ball_object.vector_x_log < 0 and ball_object.vector_y_log > 0: #ok
+            elif ball_object.vector_x_log[0] < 0 and ball_object.vector_y_log[0] > 0: #ok
                 ball_object.vector_x *= -1
-            elif ball_object.vector_x_log >= 0 and ball_object.vector_y_log < 0: #ok
+            elif ball_object.vector_x_log[0] >= 0 and ball_object.vector_y_log[0] < 0: #ok
                 ball_object.vector_y *= -1
 
         if hit_part == 'lower_left':
-            if ball_object.vector_x_log > 0 and ball_object.vector_y_log < 0: #ok
+            if ball_object.vector_x_log[0] > 0 and ball_object.vector_y_log[0] < 0: #ok
                 ball_object.vector_x *= -1
                 ball_object.vector_y *= -1
-            elif ball_object.vector_x_log > 0 and ball_object.vector_y_log > 0: #ok
+            elif ball_object.vector_x_log[0] > 0 and ball_object.vector_y_log[0] > 0: #ok
                 ball_object.vector_x *= -1
-            elif ball_object.vector_x_log <= 0 and ball_object.vector_y_log < 0: #ok
+            elif ball_object.vector_x_log[0] <= 0 and ball_object.vector_y_log[0] < 0: #ok
                 ball_object.vector_y *= -1
 
 
-    def HitBlock(self, ball_object):
+    def HitBlock(self, ball_object, combo_reseter):
         hit_list = ball_object.ball.collidelistall(self.normal_block)
         hit_num = len(hit_list)
 
         delete_block = []
 
+        if combo_reseter == True:
+            self.combo_block.clear()
+
+        #print(self.combo_block)
+
         if len(hit_list) > 2:
             self.RemainBlock(hit_list, ball_object)
+
 
         for i in hit_list:
             if ball_object.ball_x < self.normal_block[i].x and ball_object.ball_y < self.normal_block[i].y and hit_num == 1:
@@ -299,7 +313,11 @@ class Block:
         for i in hit_list:
             delete_block.append(self.block_num[i])
 
+        break_num = 0
+
         for i in delete_block:
+            break_num = break_num + 1
+
             self.block_num.remove(i)
 
             if i in self.longer_id:
@@ -311,11 +329,17 @@ class Block:
             else:
                 self.breaked_block.append('standard')
 
+            self.combo_block.append('combo')
+
+
+        return break_num
 
 
     def NumberBlock(self):   # Number of Block on field
         return len(self.block_num)
 
+    def NumberCombo(self):   # Number of Block on field
+        return len(self.combo_block)
 
 class Wall:
     def __init__(self):
@@ -352,9 +376,31 @@ class Wall:
         return game_continue
 
 
+class Score:
+    def __init__(self, x, y):
+        self.score_font = pygame.font.SysFont(None, 20)
+        self.game_score = 0
+        (self.position_x, self.position_y) = (x, y)
+
+        self.break_point = 10
+        self.combo_point = 5
+
+    def DrawScore(self, screen):
+        score_img = self.score_font.render("SCORE : " + str(self.game_score), True, (255, 200, 250))
+        screen.blit(score_img, (self.position_x, self.position_y))
+
+    def AddScore(self, break_num, combo_num):
+        if break_num > 0 and combo_num > 1:
+            self.game_score += self.break_point * break_num + self.combo_point * combo_num
+        elif break_num > 0:
+            self.game_score += self.break_point * break_num
+
+    def OutputScore(self):
+        print('SCORE :',self.game_score)
+
+
 
 def main():
-
     print('hello breakout!')
     screen_height = 800
     screen_width = 1200
@@ -372,6 +418,7 @@ def main():
     wall_object = Wall()
     shield_object = Shield()
     block_object = Block()
+    score_object = Score(40, 20)  #score position
 
     game_start = False
 
@@ -383,8 +430,7 @@ def main():
         shield_object.DrawShield(screen)
         block_object.DrawBlock(screen)
         wall_object.DrawWall(screen, screen_height, line_width)
-
-
+        score_object.DrawScore(screen)
 
         if pygame.key.get_mods() & KMOD_CTRL and game_start == False:
             game_start = True
@@ -393,40 +439,42 @@ def main():
         if game_start == False:
             ball_object.DrawVector(screen)
 
-
         pygame.display.update()
-
 
         if block_object.NumberBlock() == 0:
             pygame.mixer.music.stop()
             clear_bgm = pygame.mixer.music.load(CLEAR_BGM)
             pygame.mixer.music.play(loops = 1, start = 0.0)
             pygame.time.delay(6000)
+            score_object.OutputScore()
             print('Game clear!!')
             break
-
 
         if pygame.time.get_ticks() - past_time > ball_vel and game_start == True:
             if wall_object.HitWall(ball_object) != True or shield_object.HitShield(ball_object) != True:
                 pygame.mixer.music.stop()
                 pygame.time.delay(1000)
+                score_object.OutputScore()
                 print('Game over.')
                 break
 
             ball_object.HitException()
 
-            block_object.HitBlock(ball_object)
+            #combo_reseter = shield_object.ComboReset(ball_object)
+
+            break_num = block_object.HitBlock(ball_object, shield_object.ComboReset(ball_object))
+
+            score_object.AddScore(break_num, block_object.NumberCombo())
+
             ball_object.MoveBall()
             past_time = pygame.time.get_ticks()
 
 
         shield_object.MoveShield(block_object)
 
-        #pygame.display.update()
-
         for event in pygame.event.get():
             if event.type == QUIT:
-                print('Game over.')
+                print('Game quit.')
                 pygame.mixer.quit()
                 pygame.quit()
                 sys.exit()
